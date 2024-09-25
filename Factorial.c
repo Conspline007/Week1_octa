c860b81c19db50c0b92023166173a6d4e5e692fd
Conspline007 <thamaraikamal07@gmail.com>
2024-09-25 15:28:06 +0530

    Update Factorial.c 2

--- Factorial.c
+++ Factorial.c
@@ -3,10 +3,10 @@
 int main()
 {
 	int i,n,f=1;
-	printf("Enter the value of n");
+	printf("Enter the value of n \n");
 	scanf("%d",&n);
-	for (i=1;i<=n,i++)
-	f*=i
-	printf("The value of the factorial is %d",f);
+	for (i=1;i<=n;i++)
+	f*=i;
+	printf("The factorial of %d is %d",n,f);
 	return 0;      
 }
